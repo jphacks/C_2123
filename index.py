6608e30ba972f882e77b872e2312236ce4121f2a
takutoooooooo <taku-nishimura@g00.itscom.net>
2021-10-25 21:06:38 +0900

    ローカルでやることはできたんですが...

--- index.py
+++ index.py
@@ -1,20 +1,45 @@
 #Import necessary libraries
 from flask import Flask, render_template, Response
 import cv2
+import os
 
 camera = cv2.VideoCapture(0)
+# 分類器の指定
+cascade_path = os.path.join(
+    cv2.data.haarcascades, "haarcascade_frontalface_alt2.xml"
+)
+cascade = cv2.CascadeClassifier(cascade_path)
 
-def gen_frames():  
+ORG_WINDOW_NAME = "org"
+GAUSSIAN_WINDOW_NAME = "gaussian"
+
+def gen_frames(): 
+ 
     while True:
         success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
             ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+            print(buffer)
+            #frame = buffer.tobytes()
+            #print(frame)
+            # 画像の取得と顔の検出
+            img_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
+            face_list = cascade.detectMultiScale(img_gray, minSize=(100, 100))
+            # 検出した顔に印を付ける
+            for (x, y, w, h) in face_list:
+                color = (0, 0, 225)
+                pen_w = 3
+                cv2.rectangle(img_gray, (x, y), (x+w, y+h), color, thickness = pen_w)
+
+            # フレーム表示
+            #cv2.imshow(ORG_WINDOW_NAME, img_gray)
+            #cv2.imshow(GAUSSIAN_WINDOW_NAME, )
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
+                   b'Content-Type: image/jpeg\r\n\r\n' + img_gray + b'\r\n')
 #Initialize the Flask app
+
 
 app = Flask(__name__)
 
