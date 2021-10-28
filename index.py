88518dcae3cbfffcfcf61be3085c4b8336a7d2dd
mayu <m1716065tm@ug.jwu.ac.jp>
2021-10-28 22:44:53 +0900

    WEBページの追加

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, Response
 import cv2
 import face_recognition
 import numpy as np
+import os
 
 
 
@@ -102,9 +103,15 @@ def index():
 def video():
     return render_template('video.html')
 
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 
 
 if __name__ == "__main__":
