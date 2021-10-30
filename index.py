898d17f3794c3b06db6c376a61d5754ae59a222b
mayu <m1716065tm@ug.jwu.ac.jp>
2021-10-30 12:53:45 +0900

    インデックスの編集

--- index.py
+++ index.py
@@ -90,8 +90,8 @@ def gen_frames(known_face_names, known_face_encodings,known_face_intros):
                 cv2.rectangle(frame, (left, bottom - 70), (right, bottom), (0, 0, 255), cv2.FILLED)
                 font = cv2.FONT_HERSHEY_DUPLEX
                 cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                # cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                cv2.putText(frame, intro, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
 
             # 画像を表示する
